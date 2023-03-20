49d15986fe5a50f8ebe5e5ab27c372a462dac9ec
LuisK <luiskfibe2@gmail.com>
2023-03-19 20:29:25 -0500

    Punto 3 de la version 2.0 terminado

--- app/src/main/java/com/lcarlosfb/p3/presentation/main/MainActivity.kt
+++ app/src/main/java/com/lcarlosfb/p3/presentation/main/MainActivity.kt
@@ -5,86 +5,104 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.graphics.toColorInt
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.lcarlosfb.p3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var mainBinding: ActivityMainBinding
+	private lateinit var mainViewModel : MainViewModel
 
 	@SuppressLint("SetTextI18n")
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		mainBinding = ActivityMainBinding.inflate(layoutInflater)
+		mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 		val view = mainBinding.root
 		setContentView(view)
 
+		val msgToast = Observer<String>{toastError->
+			Toast.makeText(applicationContext,toastError,Toast.LENGTH_SHORT).show()
+		}
+		mainViewModel.toastError.observe(this, msgToast)
+
+		val msgTvGen = Observer<String>{salidaTvGen->
+			mainBinding.tvNumeroGenerado.setText(salidaTvGen)
+		}
+		mainViewModel.salidaTvGen.observe(this,msgTvGen)
+
+		val colTvGen = Observer<String> {salidaColorGen->
+			mainBinding.tvNumeroGenerado.setTextColor(salidaColorGen.toColorInt())
+		}
+		mainViewModel.salidaColorGen.observe(this, colTvGen)
+
+		val lastViewGen = Observer<Boolean> {lastOption->
+			mainBinding.edtNumber.isEnabled = lastOption
+			mainBinding.btnGenerarNum.isEnabled = lastOption
+		}
+		mainViewModel.lastOptionGen.observe(this, lastViewGen)
+
+		val nextViewGen = Observer<Boolean> {nextOption->
+			mainBinding.btnAdivinar.isEnabled = nextOption
+			mainBinding.edtAdivinar.isEnabled = nextOption
+		}
+		mainViewModel.nextOptionGen.observe(this, nextViewGen)
+
+		val msgTvAdivinar = Observer<String>{
+			mainBinding.tvResultado.setText(it)
+		}
+		mainViewModel.salidaTvAdivinar.observe(this,msgTvAdivinar)
+
+		val colTvAdivinar = Observer<String> {salidaColorAdivinar->
+			mainBinding.tvResultado.setTextColor(salidaColorAdivinar.toColorInt())
+		}
+		mainViewModel.salidaColorAdivinar.observe(this, colTvAdivinar)
+
+		val lastViewAdivinar = Observer<Boolean> {lastOptionAdivinar->
+			mainBinding.edtAdivinar.isEnabled = lastOptionAdivinar
+			mainBinding.btnAdivinar.isEnabled = lastOptionAdivinar
+		}
+		mainViewModel.lastOptionAdivinar.observe(this, lastViewAdivinar)
+
+		val nextViewAdivinar = Observer<Boolean> {nextOptionAdivinar->
+			mainBinding.btnReset.isEnabled = nextOptionAdivinar
+		}
+		mainViewModel.nextOptionAdivinar.observe(this, nextViewAdivinar)
+
+
+
 		mainBinding.btnGenerarNum.setOnClickListener {
 
-			mainBinding.edtAdivinar.setText("")
-			mainBinding.tvResultado.setText("")
-
 			val nrInput = mainBinding.edtNumber.text.toString()
+			mainViewModel.genNumber(nrInput)
 
-			if (nrInput.isEmpty()) {
-				mainBinding.tvNumeroGenerado.setText("Rellenar campos vacíos")
-				Toast.makeText(applicationContext, "Hay campos vacíos", Toast.LENGTH_SHORT).show()
-				mainBinding.tvNumeroGenerado.setTextColor("#FF0000".toColorInt())
-			}else if (nrInput.toInt() <= 1){
-				mainBinding.tvNumeroGenerado.setText("El número seleccionado debe ser mayor que 1")
-				mainBinding.tvNumeroGenerado.setTextColor("#FF0000".toColorInt())
-			}else{
-				val number = (1..nrInput.toInt()).random()
-				mainBinding.tvNumeroGenerado.setText("El número secreto ha sido generado entre: [1 y $nrInput]")
-				mainBinding.tvNumeroGenerado.setTextColor("#008000".toColorInt())
-				var contador = 0
-				mainBinding.edtNumber.isEnabled = false
-				mainBinding.btnGenerarNum.isEnabled = false
-				mainBinding.edtAdivinar.isEnabled = true
-				mainBinding.btnAdivinar.isEnabled = true
+			mainBinding.btnAdivinar.setOnClickListener {
+				val nrInputAdivinar = mainBinding.edtAdivinar.text.toString()
+				mainViewModel.adivinar(nrInputAdivinar)
 
-				mainBinding.btnAdivinar.setOnClickListener {
-					val nrValidate = mainBinding.edtAdivinar.text.toString()
-
-					if (nrValidate.isEmpty()) {
-						mainBinding.tvResultado.setText("Rellenar campos vacíos")
-						mainBinding.tvResultado.setTextColor("#FF0000".toColorInt())
-						Toast.makeText(applicationContext, "Hay campos vacíos", Toast.LENGTH_SHORT).show()
-					}else if(nrValidate.toInt() < 1 || nrValidate.toInt() > nrInput.toInt()){
-						mainBinding.tvResultado.setTextColor("#FF0000".toColorInt())
-						mainBinding.tvResultado.setText("El número ingresado está fuera del rango [1,$nrInput]")
-						mainBinding.edtAdivinar.setText("")
-					}else if (nrValidate.toInt() < number){
-						mainBinding.tvResultado.setTextColor("#FF0000".toColorInt())
-						mainBinding.tvResultado.setText("El número secreto es mayor")
-						contador += 1
-					} else if(nrValidate.toInt() > number){
-						mainBinding.tvResultado.setTextColor("#FF0000".toColorInt())
-						mainBinding.tvResultado.setText("El número secreto es menor")
-						contador += 1
-					}else{
-						contador += 1
-						mainBinding.tvResultado.setText("Adivinaste el número secreto $number, en $contador intentos" )
-						mainBinding.tvResultado.setTextColor("#008000".toColorInt())
-						mainBinding.btnAdivinar.isEnabled = false
-						mainBinding.edtAdivinar.isEnabled = false
-						mainBinding.btnReset.isEnabled = true
-
-						mainBinding.btnReset.setOnClickListener {
-							mainBinding.edtNumber.isEnabled = true
-							mainBinding.btnGenerarNum.isEnabled = true
-							mainBinding.edtNumber.setText("")
-							mainBinding.tvNumeroGenerado.setTextColor("#FF0000".toColorInt())
-							mainBinding.tvNumeroGenerado.setText("El número secreto no se ha generado")
-							mainBinding.edtAdivinar.setText("")
-							mainBinding.btnAdivinar.isEnabled = false
-							mainBinding.tvResultado.setText("")
-							mainBinding.btnReset.isEnabled = false
-						}
-					}
+				mainBinding.btnReset.setOnClickListener {
+					reset()
 				}
 			}
 		}
 	}
+
+	@SuppressLint("SetTextI18n")
+	private fun reset() {
+		mainBinding.edtNumber.isEnabled = true
+		mainBinding.btnGenerarNum.isEnabled = true
+		mainBinding.edtNumber.setText("")
+		mainBinding.tvNumeroGenerado.setTextColor("#FF0000".toColorInt())
+		mainBinding.tvNumeroGenerado.setText("El número secreto no se ha generado")
+		mainBinding.edtAdivinar.setText("")
+		mainBinding.btnAdivinar.isEnabled = false
+		mainBinding.tvResultado.setText("")
+		mainBinding.btnReset.isEnabled = false
+	}
 }
 
 //Punto 3 de la version 1.0 terminado
+
+//Punto 3 de la version 2.0 terminado
+
---
+++ app/src/main/java/com/lcarlosfb/p3/presentation/main/MainViewModel.kt
@@ -0,0 +1,83 @@
+package com.lcarlosfb.p3.presentation.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+	val toastError : MutableLiveData<String> by lazy {
+		MutableLiveData<String>()
+	}
+	val salidaTvGen : MutableLiveData<String> by lazy {
+		MutableLiveData<String>()
+	}
+	val salidaTvAdivinar : MutableLiveData<String> by lazy {
+		MutableLiveData<String>()
+	}
+	val salidaColorGen : MutableLiveData<String> by lazy {
+		MutableLiveData<String>()
+	}
+	val salidaColorAdivinar : MutableLiveData<String> by lazy {
+		MutableLiveData<String>()
+	}
+	val lastOptionGen : MutableLiveData<Boolean> by lazy {
+		MutableLiveData<Boolean>()
+	}
+	val nextOptionGen : MutableLiveData<Boolean> by lazy {
+		MutableLiveData<Boolean>()
+	}
+	val lastOptionAdivinar : MutableLiveData<Boolean> by lazy {
+		MutableLiveData<Boolean>()
+	}
+	val nextOptionAdivinar : MutableLiveData<Boolean> by lazy {
+		MutableLiveData<Boolean>()
+	}
+
+	var number = ""
+	var nInput = ""
+	var contador = 0
+
+	fun genNumber(nrInput:String){
+		if (nrInput.isEmpty()){
+			toastError.value = "Rellenar campo vacío"
+			salidaTvGen.value = "Rellenar campo vacío"
+			salidaColorGen.value = "#FF0000"
+		}else if (nrInput.toInt() <= 1){
+			salidaTvGen.value = "El número seleccionado debe ser mayor que 1"
+			salidaColorGen.value = "#FF0000"
+		}else{
+			number = (1..nrInput.toInt()).random().toString()
+			nInput = nrInput.toInt().toString()
+			salidaTvGen.value = "El número secreto ha sido generado entre: [1 y $nrInput]"
+			salidaColorGen.value = "#008000"
+			lastOptionGen.value = false
+			nextOptionGen.value = true
+		}
+	}
+
+	fun adivinar(nrInputAdivinar:String){
+
+		if(nrInputAdivinar.isEmpty()){
+			toastError.value = "Rellenar campo vacío"
+			salidaTvAdivinar.value = "Rellenar campo vacío"
+			salidaColorAdivinar.value = "#FF0000"
+		}else if(nrInputAdivinar.toInt() < 1 || nrInputAdivinar.toInt() > nInput.toInt()){
+			salidaTvAdivinar.value = "El número ingresado está fuera del rango [1,$nInput]"
+			salidaColorAdivinar.value = "#FF0000"
+		}else if (nrInputAdivinar.toInt() < number.toInt()){
+			salidaTvAdivinar.value = "El número ingresado es mayor"
+			salidaColorAdivinar.value = "#FF0000"
+			contador += 1
+		}else if (nrInputAdivinar.toInt() > number.toInt()){
+			salidaTvAdivinar.value = "El número ingresado es menor"
+			salidaColorAdivinar.value = "#FF0000"
+			contador += 1
+		}else {
+			contador += 1
+			salidaTvAdivinar.value = "Adivinaste el número secreto $number, en $contador intentos"
+			salidaColorAdivinar.value = "#008000"
+			lastOptionAdivinar.value = false
+			nextOptionAdivinar.value = true
+		}
+	}
+}
